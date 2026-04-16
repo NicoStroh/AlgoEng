f24b909dd6656cbecb228d4bec8016955e2a803e
Nico <st174452@stud.uni-stuttgart.de>
2026-04-16 19:26:49 +0200

    refactor files, added dijkstra methods for only distance and for complete path

--- core/src/graph.rs
+++ core/src/graph.rs
@@ -50,8 +50,47 @@ impl Graph {
         let end = self.incoming_offsets[node + 1];
         &&self.incoming_edges[start..end]
     }
+
+    // Only computes the difference between source and target node
+    pub fn dijkstra_distance(&self, source: usize, target: usize) -> usize {
+        let n = self.num_nodes;
+
+        let mut dist = vec![usize::MAX; n];
+        let mut prev = vec![None; n];
+
+        let mut heap = BinaryHeap::new();
+
+        dist[source] = 0;
+        heap.push((Reverse(0), source));
+
+        while let Some((Reverse(d), u)) = heap.pop() {
+            if u == target {
+                break;
+            }
+
+            if d > dist[u] {
+                continue;
+            }
+
+            for edge in self.outgoing(u) {
+                let v = edge.target;
+                let w = edge.weight;
+
+                let new_dist = d + w;
+
+                if new_dist < dist[v] {
+                    dist[v] = new_dist;
+                    prev[v] = Some(u);
+                    heap.push((Reverse(new_dist), v));
+                }
+            }
+        }
+
+        return dist[target];
+    }
     
-    pub fn dijkstra(&self, source: usize, target: usize) -> Option<(usize, Vec<usize>)> {
+    // Full dijkstra algorithm from source to target, returns the complete path
+    pub fn dijkstra_path(&self, source: usize, target: usize,) -> Option<(usize, Vec<usize>)> {
         let n = self.num_nodes;
 
         let mut dist = vec![usize::MAX; n];
